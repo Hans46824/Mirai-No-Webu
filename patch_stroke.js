d9cc39a4df6fc7add2ab30c8924a9e6d8fc847fa
Hans <PASTE_EMAIL_DI_SINI>
2026-09-18 08:37:28 +0700

    33

--- patch_stroke.js
+++
@@ -1,5 +0,0 @@
-// Read stroke.js
-const fs = require('fs');
-let code = fs.readFileSync('/home/hans/Mirai-No-Webu/js/stroke.js', 'utf8');
-
-// I will just use python instead since node is sometimes tricky without knowing setup
